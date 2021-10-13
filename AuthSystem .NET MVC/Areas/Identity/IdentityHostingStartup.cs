457d51c6556ca10e68aa80fbc5ef821df129dac9
1989deny <1989deny@gmail.com>
2021-10-13 05:01:46 +0500

    Add project files.

---
+++ AuthSystem .NET MVC/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using AuthSystem_.NET_MVC.Areas.Identity.Data;
+using AuthSystem_.NET_MVC.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(AuthSystem_.NET_MVC.Areas.Identity.IdentityHostingStartup))]
+namespace AuthSystem_.NET_MVC.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<AuthDBContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("AuthDBContextConnection")));
+
+                services.AddDefaultIdentity<ApplicationUser>(options => {
+                    options.SignIn.RequireConfirmedAccount = false;
+                    options.Password.RequireLowercase = false;
+                    options.Password.RequireUppercase = false;
+                }).AddEntityFrameworkStores<AuthDBContext>();
+            });
+        }
+    }
+}
---
+++ AuthSystem .NET MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AuthSystem_.NET_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
